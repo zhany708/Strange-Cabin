3d8c19d35b4afd4e110a0300fe4e5b1833eb9e4d
zhany708 <144260978+zhany708@users.noreply.github.com>
2023-12-27 19:32:36 -0500

    Add files via upload

---
+++ FireBat.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBat : Enemy
+{
+  
+    new void Start()
+    {
+        base.Start();
+    }
+
+   
+    new void Update()
+    {
+        base.Update();
+    }
+
+    new void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+}
