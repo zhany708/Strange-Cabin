a35e038baac4aef4630c3e47b59287e22ff5de33
zhany708 <144260978+zhany708@users.noreply.github.com>
2024-01-03 05:36:01 -0500

    Add Intermediaries

---
+++ Intermediaries/WeaponHitboxToWeapon.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHitboxToWeapon : MonoBehaviour       //用于将AggressiveWeapon脚本中的函数转交给Weapon脚本
+{
+    AggressiveWeapon m_Weapon;
+
+    private void Awake()
+    {
+        m_Weapon = GetComponentInParent<AggressiveWeapon>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        m_Weapon.AddToDetected(collision);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        m_Weapon.RemoveFromDetected(collision);
+    }
+}
