f3e20eaa31e04bd86c4ff40c94b4452871be6fd3
zhany708 <144260978+zhany708@users.noreply.github.com>
2024-01-11 09:36:14 -0500

    Update EnemyDeath.cs

--- Assets/Scripts/Enemy/EnemyDeath.cs
+++ Assets/Scripts/Enemy/EnemyDeath.cs
@@ -18,7 +18,7 @@ public class EnemyDeath : Death
     {
         base.Die();
         
-        //��Ҫʵ�֣��������������ٴ������Ŷ�������Ȼ��������������û�����žͿ���
-        m_doorController.OpenDoors();       //������������
+        //需要实现：当敌人死亡后再触发开门动画，不然敌人死亡动画还没结束门就开了。且需要加个计数用于多个怪物生成于房间时
+        m_doorController.OpenDoors();       //敌人死亡后开门
     }
 }
